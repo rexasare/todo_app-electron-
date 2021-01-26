5ba40b195deaf775f0081adbe21c9a81c2644694
Rex Asare <rex.asare06@gmail.com>
2021-01-26 12:09:18 +0000

    initial commit

---
+++ main.js
@@ -0,0 +1,87 @@
+const { BrowserView } = require("electron");
+const electron = require("electron");
+const { app, BrowserWindow, Menu, ipcMain } = electron;
+let mainWindow;
+let addWindow;
+const webPreferences = {
+  preload: `${__dirname}/preload.js`,
+};
+
+app.on("ready", () => {
+  mainWindow = new BrowserWindow({
+    webPreferences: webPreferences,
+  });
+  mainWindow.loadURL(`file://${__dirname}/main.html`);
+  mainWindow.on("closed", () => app.quit());
+
+  const mainMenu = Menu.buildFromTemplate(menuTemplate);
+  Menu.setApplicationMenu(mainMenu);
+});
+
+function createAddWindow() {
+  addWindow = new BrowserWindow({
+    width: 300,
+    height: 200,
+    title: "Add New Todo",
+    webPreferences: webPreferences,
+  });
+  addWindow.loadURL(`file://${__dirname}/add.html`);
+  addWindow.on("closed", () => (addWindow = null));
+}
+
+ipcMain.on("todo:add", (event, todo) => {
+  mainWindow.webContents.send("todo:add", todo);
+  addWindow.close();
+});
+
+function clearTodos() {
+  mainWindow.webContents.send("clear:todos");
+}
+
+const menuTemplate = [
+  {
+    label: "File",
+    submenu: [
+      {
+        label: "New Todo",
+        click() {
+          createAddWindow();
+        },
+      },
+      {
+        label: "Clear Todos",
+        click() {
+          clearTodos();
+        },
+      },
+      {
+        label: "Quit",
+        accelerator: process.platform == "darwin" ? "Command+Q" : "Ctrl+Q",
+        click() {
+          app.quit();
+        },
+      },
+    ],
+  },
+];
+
+if (process.platform == "darwin") {
+  menuTemplate.unshift({ label: "" });
+}
+
+if (process.env.NODE_ENV !== "production") {
+  menuTemplate.push({
+    label: "View",
+    submenu: [
+      { role: "reload" },
+      {
+        label: "Toggle Developer Tools",
+        accelerator:
+          process.platform == "darwin" ? "Command+Alt+I" : "Ctrl+Shift+I",
+        click(item, focusedWindow) {
+          focusedWindow.toggleDevTools();
+        },
+      },
+    ],
+  });
+}
